3b1d20ef2b984f0ec537462db4887ccc65887ec1
Sanive <santhosk1019@gmail.com>
2024-08-26 16:57:52 +0530

    feat : added a java file

---
+++ hello.java
@@ -0,0 +1,5 @@
+class hello { // Defines a class named 'hello'
+    public static void main(String[] args) { // The main method, which is the entry point of the program
+        System.out.println("Hello world"); // Prints "Hello world" to the console
+    }
+}
